f97a7ecc04d6a6f14c99433357e0932313c21e0d
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-17 10:36:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  index = index.to_i - 1
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  elsif board[index] == " X " || board[index] == "O"
-    true
   else
-    return
+    true
   end
 end
